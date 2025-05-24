e9ec0ef42e32eae8c751b82cf619e6639bad0436
Itay Leon <itayleon81@walla.com>
2025-05-24 03:45:00 +0300

    fix: add cors dependency and its types to package.json

--- wallet-server/src/app.ts
+++ wallet-server/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
 import passRouter from "./router/pass";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); 
 
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.url}`);
