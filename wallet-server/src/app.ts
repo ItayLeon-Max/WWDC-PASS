d516deaead0308a06e8c3aed92438a666ff47099
Itay Leon <itayleon81@walla.com>
2025-05-24 03:21:18 +0300

    fix: clean up app.ts by removing duplicate middleware registrations

--- wallet-server/src/app.ts
+++ wallet-server/src/app.ts
@@ -5,11 +5,11 @@ import passRouter from "./router/pass";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
-// app.use(passRouter);
+app.use(express.static("public")); 
+
 app.use("/pass", passRouter); 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
