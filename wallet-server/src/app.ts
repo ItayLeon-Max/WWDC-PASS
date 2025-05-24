802daf2aaa80c25dca586e34aaa5db2b4523b576
Itay Leon <itayleon81@walla.com>
2025-05-24 03:32:36 +0300

    fix: reorder imports and standardize static file serving path

--- wallet-server/src/app.ts
+++ wallet-server/src/app.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import passRouter from "./router/pass";
+import path from "path"; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(bodyParser.json());
-app.use(express.static("public")); 
 
-app.use("/pass", passRouter); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/pass", passRouter);
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
